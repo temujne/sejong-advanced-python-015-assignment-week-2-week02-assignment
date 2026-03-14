59692fe724038a20836fe5e990e68f228129e603
Temuujin <temujne@gmail.com>
2026-03-15 00:32:32 +0900

    25014223

--- assignment.py
+++ assignment.py
@@ -1,1 +1,32 @@
+#1 First
+length = int(input("Enter length "))
+width = int(input("Enter width "))
 
+area = length*width
+print("Area = ", area)
+
+
+#2 Second
+n1 = int(input("Enter integer "))
+n2 = float(input("Enter float "))
+
+n3 = n1 + n2
+print(n3)
+print("data type:", type(n3))
+
+#int+float=float
+
+#3 Three
+name = input("Enter name ")
+age = input("Age ")
+gender = input("Enter gender ")
+height = int(input("Enter height "))
+
+print("User information:\n")
+print("User name:", name)
+print("Age:", age)
+print("User gender:", gender)
+print("User height:", height)
+
+
+#25014223 Temuujin
